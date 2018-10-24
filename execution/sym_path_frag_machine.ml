d50117e4773790072630c4e9240796ecc233cedc
yanxx297 <yanxx297@umn.edu>
2018-10-23 20:24:10 -0500

    Move symbolic memory tracing related code to sym path fragment machine Given that -trace-sym-mem only make sense for symbolic memory, and that fragment_machine is for concrete memory, we move those code to sym_path_frag_machine

--- execution/sym_path_frag_machine.ml
+++ execution/sym_path_frag_machine.ml
@@ -874,11 +874,80 @@ struct
 	      opt_trace_taint := ta)
 	!opt_trace_detailed_ranges
 	  
+    val mutable sym_mem = []
+    val mutable sym_mem_update = []
+    val mutable prog_form = Hashtbl.create 10
+
+    method log_sym_update addr len =
+      let rec loop l l1 h1 = 
+        match l with
+          | h::rest ->
+              (let (l2, h2) = h in
+                 if l1 > h2 then loop rest l1 h1
+                 else if h1 < l2 then ()
+                 else 
+                   let l' = if l1 > l2 then l1 else l2 in
+                   let h' = if h1 > h2 then h2 else h1 in
+                   let len' = h' - l' in
+                     if len' > 0 then
+                       (for offset = 0 to (len'-1) do
+                          let item = ((Int64.of_int (l'+offset)), V.REG_8) in
+                            if not (List.exists (fun e ->
+                                                   if e = item then true else false)
+                                      sym_mem_update) then
+                              sym_mem_update <- item::sym_mem_update;
+                        done;
+                        if h' != h1 then loop l h' h1)
+              )
+          | [] -> ()
+      in
+        loop sym_mem (Int64.to_int addr) (Int64.to_int (Int64.add addr len))
+                
+    method add_sym_mem base len = 
+      Printf.printf "Add sym mem 0x%Lx(+%d)\n" base (Int64.to_int len);
+      let rec merge l l1 h1 = 
+        match l with
+          | h::rest -> 
+              (let (l2, h2) = h in
+                 if l1 > h2 then
+                   h::(merge rest l1 h1)
+                 else if h1 < l2 then
+                    (l1, h1)::l
+                 else
+                   let l' = if l1 > l2 then l2 else l1 in
+                   let h' = if h1 > h2 then h1 else h2 in
+                     merge rest l' h'
+              )
+          | [] ->
+              [(l1, h1)]
+      in
+        sym_mem <- merge sym_mem (Int64.to_int base) (Int64.to_int (Int64.add base len));
+        let str = ref "" in
+          List.iter (fun (l, h) ->
+                         str := !str ^ (Printf.sprintf "(0x%Lx, 0x%Lx) " (Int64.of_int l) (Int64.of_int h))
+          ) sym_mem;
+          Printf.printf "%s\n" !str
+
     method finish_path =
-      ignore(fm#finish_path);
       dt#set_heur 1;
       dt#mark_all_seen;
       infl_man#finish_path;
+      if !opt_trace_sym_mem then
+        (let rec loop l =
+           match l with
+             | h::rest -> 
+                 if rest = [] then h else V.BinOp(V.BITAND, h, (loop rest))
+             | [] -> failwith ""
+         in
+           List.iter (fun (addr, _) ->
+                        let e = D.to_symbolic_8 (self#load_byte addr) in
+                        let old_form = 
+                          (try Hashtbl.find prog_form addr with
+                             | Not_found -> V.Constant(V.Int(V.REG_8, 0L)) 
+                          ) in
+                          Hashtbl.replace prog_form addr 
+                            (V.Ite((loop self#get_path_cond), e, old_form))
+           ) sym_mem_update);
       if !opt_trace_binary_paths then
 	Printf.printf "Path: %s\n" dt#get_hist_str;
       if !opt_trace_binary_paths_delimited then
@@ -903,6 +972,7 @@ struct
 
     method reset () =
       fm#reset ();
+      sym_mem_update <- [];
       form_man#reset_mem_axioms;
       path_cond <- [];
       V.VarHash.clear var_seen_hash;
@@ -914,7 +984,15 @@ struct
 
     method after_exploration =
       infl_man#after_exploration;
-      fm#after_exploration;
+      if !opt_trace_sym_mem then
+        let str = ref "memory update:" in
+          List.iter (fun (l, ty) ->
+                       str := !str^Printf.sprintf "(0x%Lx, %s) " l (V.type_to_string ty)
+          ) sym_mem_update;
+          Printf.printf "%s\n" !str;
+          Hashtbl.iter (fun addr e ->
+                          Printf.printf "mem[0x%Lx] = %s\n" addr (V.exp_to_string e)
+          ) prog_form;
       if !opt_trace_working_ce_cache then
 	Printf.printf "CE cache stats: %Ld hits / %Ld refs\n"
 	  !ce_cache_hits !ce_cache_refs
