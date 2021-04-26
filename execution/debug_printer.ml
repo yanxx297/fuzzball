080ba4e997d33d96abeb08eadd64484f59207e0e
yanxx297 <yanxx297@umn.edu>
2021-04-26 01:47:06 -0500

    debug: add more printers for ocamldebug

--- execution/debug_printer.ml
+++ execution/debug_printer.ml
@@ -9,3 +9,11 @@ let print_loopbody fmt loop_body =
 let print_eip fmt (n: DS.elt) = Format.fprintf fmt "0x%Lx" n
 
 let print_expr fmt expr = Format.fprintf fmt "%s" (Vine.exp_to_string expr)
+
+let print_lval fmt lval = Format.fprintf fmt "%s" (Vine.lval_to_string lval)
+
+let print_decl fmt t = 
+  Format.fprintf fmt "decl size: %d\n" (Hashtbl.length t);
+  Hashtbl.iter (fun lval eip ->
+                  Format.fprintf fmt "%s %Lx\n" (Vine.lval_to_string lval) eip)
+    t
--- execution/debug_printer.mli
+++ execution/debug_printer.mli
@@ -2,3 +2,5 @@ module DS: (Set.S with type elt=int64)
 val print_loopbody: Format.formatter -> (int64, unit) Hashtbl.t -> unit
 val print_eip: Format.formatter -> DS.elt -> unit
 val print_expr : Format.formatter -> Vine.exp -> unit
+val print_lval : Format.formatter -> Vine.lvalue -> unit
+val print_decl: Format.formatter -> (Vine.lvalue, int64) Hashtbl.t -> unit
